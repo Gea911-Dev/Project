87f3767e3590ea30481284c7212559ccb49f8a4f
King Gea <vicgeanafilrully19@gmail.com>
2026-04-26 00:48:35 +0700

    fix-in dulu

--- SIGAP_Gentong/assets/js/data.js
+++ SIGAP_Gentong/assets/js/data.js
@@ -1,4 +1,4 @@
-const wisataData = [
+const locations = [
     {
         "nama": "Sumber Gentong",
         "lat": -7.9386,
@@ -6,10 +6,16 @@ const wisataData = [
         "deskripsi": "Wisata sumber air jernih dengan pemandangan alam yang asri."
     },
     {
+        "nama": "Coban Talun",
+        "lat": -7.868,
+        "lng": 112.5016,
+        "deskripsi": "Air terjun indah dengan taman bunga dan area perkemahan."
+    },
+    {
         "nama": "Pantai Balekambang",
         "lat": -8.4049,
         "lng": 112.5394,
-        "deskripsi": "Pantai indah dengan pura di atas pulau karang, mirip Tanah Lot."
+        "deskripsi": "Pantai dengan pura di atas pulau karang, mirip Tanah Lot."
     },
     {
         "nama": "Gunung Bromo",
@@ -19,14 +25,8 @@ const wisataData = [
     },
     {
         "nama": "Jatim Park 3",
-        "lat": -7.8889,
-        "lng": 112.5616,
-        "deskripsi": "Taman hiburan modern dengan tema Dinosaurus dan berbagai wahana."
-    },
-    {
-        "nama": "Coban Rondo",
-        "lat": -7.8917,
-        "lng": 112.4732,
-        "deskripsi": "Air terjun yang indah dengan taman labirin di sekitarnya."
+        "lat": -7.8883,
+        "lng": 112.561,
+        "deskripsi": "Taman hiburan modern dengan tema dinosaurus dan berbagai wahana."
     }
 ];
