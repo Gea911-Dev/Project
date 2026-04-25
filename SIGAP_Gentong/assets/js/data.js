b3d30124d0eb3430bc3462c44a8fd226eda863bf
King Gea <vicgeanafilrully19@gmail.com>
2026-04-25 23:24:36 +0700

    first commit

---
+++ SIGAP_Gentong/assets/js/data.js
@@ -0,0 +1,32 @@
+const wisataData = [
+    {
+        "nama": "Sumber Gentong",
+        "lat": -7.9386,
+        "lng": 112.6785,
+        "deskripsi": "Wisata sumber air jernih dengan pemandangan alam yang asri."
+    },
+    {
+        "nama": "Pantai Balekambang",
+        "lat": -8.4049,
+        "lng": 112.5394,
+        "deskripsi": "Pantai indah dengan pura di atas pulau karang, mirip Tanah Lot."
+    },
+    {
+        "nama": "Gunung Bromo",
+        "lat": -7.9425,
+        "lng": 112.9533,
+        "deskripsi": "Gunung berapi aktif yang terkenal dengan pemandangan matahari terbitnya."
+    },
+    {
+        "nama": "Jatim Park 3",
+        "lat": -7.8889,
+        "lng": 112.5616,
+        "deskripsi": "Taman hiburan modern dengan tema Dinosaurus dan berbagai wahana."
+    },
+    {
+        "nama": "Coban Rondo",
+        "lat": -7.8917,
+        "lng": 112.4732,
+        "deskripsi": "Air terjun yang indah dengan taman labirin di sekitarnya."
+    }
+];
